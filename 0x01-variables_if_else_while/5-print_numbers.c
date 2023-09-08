bd468870b1cb072eeb374fed771e5b97af0b8fb5
Sh-in3 <majorshine14@gmail.com>
2023-09-08 15:51:23 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always O (Success)
+*/
+
+int main(void)
+{
+ 	int i = 0;
+
+	while (i < 10)
+	{
+		printf(i)
+	}
+	++i
+}
