d761216e437d00c69bfc3c00391454658792b3dd
Sh-in3 <majorshine14@gmail.com>
2023-09-08 15:35:57 -0700

    uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,12 +8,19 @@
 int main(void)
 {
 	char i = 'a';
+	char I = 'A'
 
-	while (i <= 'Z')
+	while (i <= 'z')
 	{
 		putchar(i);
 		++i;
 	}
+	while (I <= 'Z')
+	{
+		putchar(I);
+		++I;
+	}
+
 	putchar('\n');
 	return (0);
 }
