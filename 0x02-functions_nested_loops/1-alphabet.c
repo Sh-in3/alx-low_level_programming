62e394f11751e1889c2eb18595120a612e075aeb
Sh-in3 <majorshine14@gmail.com>
2023-09-12 12:51:56 -0700

    aalpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include 'main.h'
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char j = 97;
+	int i = 0;
+	
+	for (i; i < 26;)
+	{
+		_putchar("%c \n", j + i);
+		i++;
+	}
+}
