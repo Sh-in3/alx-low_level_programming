666c689bfadf3038dc04346040d5db4e749b4b77
Sh-in3 <majorshine14@gmail.com>
2023-09-12 14:56:09 -0700

    print 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/* print_alphabet_x10 - print the iterations of a -z 10x
+ * Return: always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0, ch;
+
+	while (i < 10;)
+	{
+		for (ch = 'a'; ch <= 'z'; ++ch)
+		_putchar(ch);
+	_putchar('\n');
+	i++;
+	}
+}
