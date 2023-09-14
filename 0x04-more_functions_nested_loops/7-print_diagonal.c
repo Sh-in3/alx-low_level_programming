f253fd37fb0f44e957362a70a8528e69d67f9d18
Sh-in3 <majorshine14@gmail.com>
2023-09-14 15:45:13 -0700

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include"main.h"
 
 /**
  * print_diagonal - print a diagonal line
@@ -9,20 +9,17 @@
 
 void print_diagonal(int n)
 {
-	int i, space;
+	int postn, space;
 
 	if (n <= 0)
-	{
-		_putchar ('\n');
-	}
-
+		_putchar('\n');
 	else
 	{
-		for (i = 1; i <= n; ++i)
+		for (postn = 1; postn <= n; ++postn)
 		{
-			for (space = 1; space <= i; ++space)
+			for (space = 1; space <= postn; ++space)
 				_putchar(' ');
-			_putchar('\\');
+			_putchar(92); /*is equal to '/' char*/
 			_putchar('\n');
 		}
 	}
