0bbff192c4654f3ace77a30a6b35d6975edac4d7
Sh-in3 <majorshine14@gmail.com>
2023-09-14 12:19:51 -0700

    printnumbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	int i;
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + 48);
+	}
+	_putchar('\n');
+}
