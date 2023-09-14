a797a3cf8b81edd245b138e154affb7ad1400c7e
Sh-in3 <majorshine14@gmail.com>
2023-09-14 15:56:45 -0700

    prime

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,6 +1,30 @@
 #include<stdio.h>
 
 /**
+ * _sqrt - finds the square root
+ *
+ * @x: input number
+ *
+ * Return: square root of x
+ *
+*/
+
+double _sqrt(double x)
+{
+	float sqrt, tmp;
+
+	sqrt = x / 2;
+	tmp = 0;
+
+	while (sqrt != tmp)
+	{
+		tmp = sqrt;
+		sqrt = (x / tmp + tmp) / 2;
+	}
+	return (sqrt);
+}
+
+/**
  * largest_prime_factor - finds and prints the largest
  *			prime factor of number (num)
  *
@@ -29,4 +53,17 @@ void largest_prime_factor(long int num)
 	if (num > 2)
 		largest = num;
 	printf("%d\n", largest);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+
+	largest_prime_factor(612852475143);
+
+	return (0);
 }
