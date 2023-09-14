c041aa21631399af8587d017d3e5142e4e8b6843
Sh-in3 <majorshine14@gmail.com>
2023-09-14 14:45:52 -0700

    print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* print_line - print a line
+*
+* @n: line length in characters
+*
+* Return: Always 0
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	if (n == 0)
+	{
+		_putchar ('\n');
+	}
+
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar ('_');
+		}
+		_putchar ('\n');
+	}
+}
