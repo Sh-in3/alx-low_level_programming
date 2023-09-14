f543dad304d453c53d680b2b9a6a2296ab13531e
Sh-in3 <majorshine14@gmail.com>
2023-09-14 12:23:25 -0700

    numbers 0 - 9[5~

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_numbers - print nubers from 0 -9
  *
  * Return: Always 0.
  */
@@ -9,6 +9,7 @@
 void print_numbers(void)
 {
 	int i;
+
 	for (i = 0; i <= 9; i++)
 	{
 		_putchar(i + 48);
