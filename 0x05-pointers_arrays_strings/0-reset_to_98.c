1cd885da3f4ebf995298ee7b4cd5acc899883c42
Sh-in3 <majorshine14@gmail.com>
2023-09-18 13:38:59 -0700

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updae the value
+ *
+ * @n: value to be checked
+ * Return: void function doesn't require a return
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
