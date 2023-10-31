bcae8a30309bf6c9182ff735b30bc3268b98326e
Sh-in3 <majorshine14@gmail.com>
2023-10-31 11:20:45 -0700

    4

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,48 +9,40 @@
  *
  * Return: 0 if it fails or actual number of letters it could
  *         read and print
- */
-
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	if (filename == NULL)
-	{
+	int file;
+	ssize_t read_check, wcount;
+	char *buffer;
+
+	if (filename == NULL) /*check if file is present*/
 		return (0);
-	}
 
-	int file_descriptor = open(filename, O_RDONLY);
+	file = open(filename, O_RDONLY); /*open file*/
 
-	if (file_descriptor == -1)
-	{
+	if (file == -1)
 		return (0);
-	}
 
-	char *buffer = (char *)malloc(letters);
-
+	/*get the size of buffer from number of letters*/
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
-		close(file_descriptor);
-		return (0);
-	}
-
-	ssize_t bytes_read = read(file_descriptor, buffer, letters);
-
-	if (bytes_read == -1)
-	{
-		close(file_descriptor);
 		free(buffer);
 		return (0);
 	}
 
-	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-
-	if (bytes_written == -1 || bytes_written != bytes_read)
-	{
-		close(file_descriptor);
-		free(buffer);
+	read_check = read(file, buffer, letters); /*read file*/
+	if (read_check == -1) /*check if read failed*/
 		return (0);
-	}
-	close(file_descriptor);
+
+	wcount = write(STDOUT_FILENO, buffer, read_check); /*write to POSIX*/
+	if (wcount == -1 || read_check != wcount) /*check if write failed*/
+		return (0);
+
 	free(buffer);
-	return (bytes_written);
+
+	close(file); /*close file*/
+
+	return (wcount);
 }
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,33 +7,30 @@
  * @text_content: string to write to file
  *
  * Return: 1 on success OR -1 on faliure
- */
-
+*/
 int create_file(const char *filename, char *text_content)
 {
-	if (filename == NULL)
-	{
+	int file, write_status, words = 0;
+
+	if (filename == NULL) /*check if filename is present*/
 		return (-1);
-	}
 
-	int file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (file_descriptor == -1)
-	{
+	/*open file by creating it and if it exists write but truncate to 0*/
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (file == -1) /*check if file creation was a success*/
 		return (-1);
-	}
 
-	if (text_content != NULL)
+	if (text_content) /*write content to file if its not NULL*/
 	{
-		ssize_t bytes_written = write(file_descriptor, text_content, strlen(text_content));
+		while (text_content[words] != '\0') /*find number of words*/
+			words++;
 
-		if (bytes_written == -1)
-		{
-			close(file_descriptor);
+		/*write to file*/
+		write_status = write(file, text_content, words);
+		if (write_status == -1) /*check if write was a success*/
 			return (-1);
-		}
 	}
 
-	close(file_descriptor);
+	close(file); /*close file*/
 	return (1);
 }
